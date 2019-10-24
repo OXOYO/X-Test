a6fd141bb1d9a0921de3122ab5a4bc1b712b8128
OXOYO <zmn2007.hi@163.com>
2019-10-24 14:52:02 +0800

    update document。

--- Curry/index.js
+++ Curry/index.js
@@ -5,9 +5,19 @@
 * 用闭包把参数保存起来，当参数的数量足够执行函数了，就开始执行函数
 * */
 
-
+// 柯里化
 var curry = fn =>
   judge = (...args) =>
     args.length === fn.length
       ? fn(...args)
       : (arg) => judge(...args, arg)
+
+// 测试
+var fn = curry(function (a, b, c) {
+  console.log([a, b, c])
+})
+
+fn('a', 'b', 'c') // ['a', 'b', 'c']
+fn('a', 'b')('c') // ['a', 'b', 'c']
+fn('a')('b')('c') // ['a', 'b', 'c']
+fn('a')('b', 'c') // ['a', 'b', 'c']
