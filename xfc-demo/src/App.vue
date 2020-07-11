5f1cdd1c31d4b54296e4489ca930ae0ccdf11095
oxoyo <zmn2007.hi@163.com>
2020-07-11 23:33:45 +0800

    feat:添加@oxoyo/xf包使用示例。

---
+++ xfc-demo/src/App.vue
@@ -0,0 +1,45 @@
+<style lang="less">
+</style>
+
+<template>
+  <div>
+    <div class="editor" id="editor"></div>
+  </div>
+</template>
+
+<script>
+  // import G6 from '@antv/g6'
+  // import Vue from 'vue'
+  // import Vuex from 'vuex'
+  // import iView from 'iview'
+  // Vue.use(iView)
+  // window.Vue = Vue
+  // import 'iview/dist/styles/iview.css'
+  // import xfc from './xfc/xfc.umd.min'
+  // import './xfc/xfc.css'
+  import xfc from '@oxoyo/xfc'
+  import '@oxoyo/xfc/dist/xfc.css'
+
+  export default {
+    name: 'app',
+    mounted () {
+      const xfcEditor = xfc({
+        el: '#editor',
+        props: {
+          i18n: {
+            // 默认语言包
+            defLocale: 'en-US',
+            // 自定义语音包或覆写系统语音包
+            locales: {
+              'zh-CN': {
+                L10001: '111',
+                L10002: '222'
+              }
+            }
+          }
+        }
+      })
+      console.log('xfcEditor', xfcEditor)
+    }
+  }
+</script>
